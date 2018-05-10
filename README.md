4653b7eb6fea2a8f751bbb2b442deff26c4acbb7
Alejandro <0xalen@protonmail.com>
2018-05-10 05:16:31 -0300

    Free Software disclaimer added

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# tallerpy3
-Colección de ejemplos y código de presentación para dictado de un taller de Python 3
+# TallerPy3
+
+TallerPy3 es una colección de scripts que presenta conceptos fundamentales para
+una primera aproximación a Python. Se incluyen ejemplos que ilustran el uso y 
+las convenciones que dictan el buen estilo de programación.
+
+TallerPy3 se diseño con el dictado de un taller de Python 3 en mente.
--- operaciones.py
+++ operaciones.py
@@ -1,5 +1,22 @@
-# operaciones.python
 #!/usr/bin/python3
+# -*- coding:utf-8 -*-
+#
+# Copyright 2018 Alejandro Trinidad <franco.unpa@protonmail.com>
+#
+# TallerPy3 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# These scripts are distributed in the hope that they will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with TallerPy3 . If not, see <http://www.gnu.org/licenses/>.
+#
+
 from random import randint
 
 def obtenerNumero():
